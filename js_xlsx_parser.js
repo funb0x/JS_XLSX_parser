7dd62a5dfc353fb791e5a19276c5d96edac78d47
Oleg <oleg.tymchak@gmail.com>
2019-03-27 00:33:15 +0100

    ref

--- js_xlsx_parser.js
+++ js_xlsx_parser.js
@@ -1,15 +1,14 @@
 var XLSX = require('xlsx');
-var workbook = XLSX.readFile('C:/Users/Oleg_Tymchak/Desktop/01_09_18_27_12_18 - Copy.xlsx');
+var workbook = XLSX.readFile('C:/01_09_18_27_12_18 - Copy.xlsx');
 
-var sheet_name_list = workbook.SheetNames;
-
-sheet_name_list.forEach(function(y) {
+var data = [];
+workbook.SheetNames.forEach(function(y) {
     var worksheet = workbook.Sheets[y];
     var headers = {};
-    var data = [];
+    
     for(var z in worksheet) {
         if(z[0] === '!') continue;
-        //console.log(z);
+        
         //parse out the column, row, and value
         var col = z.substring(0,1);
         var row = parseInt(z.substring(1));
@@ -23,13 +22,35 @@ sheet_name_list.forEach(function(y) {
 
         if(!data[row]) data[row]={};
         data[row][headers[col]] = value;
-        
-        //console.log(col);
-        //console.log(row);
-        //console.log(value);
     }
     //drop those first two rows which are empty
     data.shift();
     data.shift();
-    console.log(data);
+    //console.log(data);
 });
+
+var result = {};
+
+data.forEach(function(entry) {
+    
+    if (!result[entry.Name]) result[entry.Name] = {};
+    var employee = result[entry.Name];
+
+    var date = new Date(entry.time);
+    var dateString = date.toLocaleDateString();
+    
+    if (!employee[dateString]) employee[dateString] = {};
+    var dateEntry = employee[dateString];
+
+    if (!dateEntry.totalTime) dateEntry.totalTime = 0;
+    
+    if (!dateEntry.date) {
+        dateEntry.date = date;
+    } else {
+        dateEntry.totalTime += date - dateEntry.date; 
+    }
+    
+
+})
+
+console.log(result)
